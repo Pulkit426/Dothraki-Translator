0fd16294f52e2a14eff21dae1530ccd3a033314f
Pulkit426 <agrawalpulkit12@gmail.com>
2021-11-09 00:16:56 +0530

    fetch call

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outText= document.querySelector("#out-text");
+var serverUrl ="https://api.funtranslations.com/translate/dothraki.json"
+function getTranslationUrl(input){
+    return serverUrl +"?text=" + input;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong happened");
+}
+
 function clickEvent(){
-    outText.innerText="Translated  " + txtInput.value;
+    //outText.innerText="Translated  " + txtInput.value;
+    fetch(getTranslationUrl(txtInput.value))
+    .then(response =>response.json())
+    .then(json => outText.innerText=json.contents.translated)
+    .catch(errorHandler);
 }
 
 
